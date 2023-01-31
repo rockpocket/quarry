358aceace642b295e04932198f9772322e4bea3b
drobnicc <drobnicc@oclc.org>
2023-01-31 10:26:00 -0500

    boulder

--- socket2me.py
+++ socket2me.py
@@ -9,3 +9,5 @@ socket.bind(cfgmgr09sxm1.shr.oclc.org, 25)
 socket.listen()
 
 socket.accept()
+
+#https://www.tutorialspoint.com/python_penetration_testing/python_penetration_testing_socket_and_methods.htm
