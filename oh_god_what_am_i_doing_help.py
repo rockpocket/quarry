146558032e6dc967e1cabe30aa2a958ab12237f8
drobnicc <drobnicc@oclc.org>
2023-02-03 13:53:37 -0500

    fml

---
+++ oh_god_what_am_i_doing_help.py
@@ -0,0 +1,14 @@
+#run, dear god run for you life
+
+variable = 'integer'
+string = True
+boolean = 1.0
+float = False
+integer = '69 lol'
+
+print("The variable's data type is",type(variable))
+print("The string's data type is",type(string))
+print("The boolean's data type is",type(boolean))
+print("The float's data type is",type(float))
+print("The integer's data type is",type(integer))
+print("I want to fucking kill myself")
