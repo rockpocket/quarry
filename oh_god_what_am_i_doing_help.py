18ae7d1bfa3f8094983e4b58ed023d9f95e2a20a
drobnicc <drobnicc@oclc.org>
2023-02-03 14:10:38 -0500

    fml

--- oh_god_what_am_i_doing_help.py
+++ oh_god_what_am_i_doing_help.py
@@ -6,9 +6,9 @@ boolean = 1.0
 float = False
 integer = '69 lol'
 
-print("The variable's data type is",type(variable))
-print("The string's data type is",type(string))
-print("The boolean's data type is",type(boolean))
-print("The float's data type is",type(float))
-print("The integer's data type is",type(integer))
+print("The variable's data type is",type(variable),"it's object value is",variable)
+print("The string's data type is",type(string),"it's object value is",string)
+print("The boolean's data type is",type(boolean),"it's object value is",boolean)
+print("The float's data type is",type(float),"it's object value is",float)
+print("The integer's data type is",type(integer),"it's object value is",integer)
 print("I want to fucking kill myself")
