e5ee843ae0a866d77e69b22576693272e549ff07
drobnicc <drobnicc@oclc.org>
2023-02-07 11:27:21 -0500

    fml

--- oh_god_what_am_i_doing_help.py
+++ oh_god_what_am_i_doing_help.py
@@ -3,17 +3,23 @@
 import pandas as pd
 import re
 
+sr = pd.Series(['New_Yokr', 'Libson', 'Tokyo', 'Paris', 'Munich'])
+
+idx = ['City 1', 'City 2', 'City 3', 'City 4', 'City 5']
+
+sr.index = idx
+
+print(sr)
 
 
 variable = 'integer'
 string = True
 boolean = 1.0
 float = False
-integer = '69 lol'
+integer = '123'
 
 #print("The variable's data type is",type(variable),"it's object value is",variable)
 #print("The string's data type is",type(string),"it's object value is",string)
 #print("The boolean's data type is",type(boolean),"it's object value is",boolean)
 #print("The float's data type is",type(float),"it's object value is",float)
-print("The integer's data type is",type(integer),"it's object value is",integer)
-#print("I want to fucking kill myself")
+#print("The integer's data type is",type(integer),"it's object value is",integer)
