506561133e9cf691c6358addd4713711492534ee
drobnicc <drobnicc@oclc.org>
2023-02-07 10:52:59 -0500

    fml

--- oh_god_what_am_i_doing_help.py
+++ oh_god_what_am_i_doing_help.py
@@ -1,14 +1,19 @@
 #run, dear god run for you life
 
+import pandas as pd
+import re
+
+
+
 variable = 'integer'
 string = True
 boolean = 1.0
 float = False
 integer = '69 lol'
 
-print("The variable's data type is",type(variable),"it's object value is",variable)
-print("The string's data type is",type(string),"it's object value is",string)
-print("The boolean's data type is",type(boolean),"it's object value is",boolean)
-print("The float's data type is",type(float),"it's object value is",float)
+#print("The variable's data type is",type(variable),"it's object value is",variable)
+#print("The string's data type is",type(string),"it's object value is",string)
+#print("The boolean's data type is",type(boolean),"it's object value is",boolean)
+#print("The float's data type is",type(float),"it's object value is",float)
 print("The integer's data type is",type(integer),"it's object value is",integer)
-print("I want to fucking kill myself")
+#print("I want to fucking kill myself")
