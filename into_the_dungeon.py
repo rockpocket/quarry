a59456c042dc6f4fd2aee397a64e69e1c05414ef
rockpocket <sgtdrobnick@gmail.com>
2023-03-06 20:29:52 -0500

    oy

--- into_the_dungeon.py
+++ into_the_dungeon.py
@@ -1,1 +1,8 @@
 player = input("Welcome Adventurer, to The Dungeon!\nFirst off, may I ask your name?\n-")
+
+
+print(player)
+
+if player == "no" or "No" or "":
+    print("Please don't be difficult, I'm at work right now. Name, please")
+#    if player
