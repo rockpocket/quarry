e299c3a4bc76ce48c309593ae0d71de9a29b8b62
rockpocket <sgtdrobnick@gmail.com>
2023-03-13 22:07:11 -0400

    oy

--- into_the_dungeon.py
+++ into_the_dungeon.py
@@ -1,7 +1,14 @@
-player = input("Welcome Adventurer, to The Dungeon!\nFirst off, may I ask your name?\n-")
+player = input("Welcome Adventurer, to The Dungeon!\nFirst off, may I ask your name?\n")
 
 
-if (player == "no"):
+if player == "no":
+    print("Please don't be difficult, I'm this is me working right now. Name, please")
+elif (player is None):
     print("Please don't be difficult, I'm this is me working right now. Name, please")
 else:
     print(player + ", Wonderful! What a grand name, not stupid at all.")
+
+role = input("Now, let's begin, first, choose a class: Fighter, Wizard, or Crackhead?\n")
+
+if role == "Wizard"
+    "Ah, so the "
