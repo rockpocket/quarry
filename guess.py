8f6ba88f8645d403d205aee615ce73dda649b046
drobnicc <drobnicc@oclc.org>
2023-09-14 09:56:57 -0400

    fml

--- guess.py
+++ guess.py
@@ -1,7 +1,16 @@
+import sys
+
 secret = 7
 
 guess = input("what's you're guess?")
 
+guess = int(guess)
+
 if (guess < secret):
-    then print("Too low, fuck-o")
+    print("Too low, fuck-o")
 elif (guess > secret):
+    print("Too high, smart guy")
+elif (guess == secret):
+    print("accuarate and immaculate")
+
+sys.exit(0) > None
---
+++ practice.py
@@ -0,0 +1,6 @@
+bananas = 5
+cost = 2
+
+total = bananas * cost
+
+print(total)
