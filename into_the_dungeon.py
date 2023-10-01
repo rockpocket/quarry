15f83453d9880329b296954177f045a91581041c
drobnicc <drobnicc@oclc.org>
2023-10-01 00:42:17 -0400

    fml

--- into_the_dungeon.py
+++ into_the_dungeon.py
@@ -1,3 +1,6 @@
+import re
+import time
+
 player = input("Welcome Adventurer, to The Dungeon!\nFirst off, may I ask your name?\n")
 
 
@@ -8,11 +11,26 @@ elif (player is None):
 else:
     print(player + ", Wonderful! What a grand name, not stupid at all.")
 
-role = input("Now, let's begin, first, choose a class: Fighter, Wizard, or Crackhead?\n")
+role = input("Now, let's begin, first, choose a class: fighter, wizard, or crackhead?\n")
 
 role = role.casefold()
 
-if role == "wizard":
-    print("Ah, so the spell slinging wizard, wise choice, or so, we shall see.")
+match role:
+    case "wizard":
+        print("Ah, so the spell slinging wizard, wise choice, or so, we shall see.")
+    case "fighter":
+        print("Sturdy and deadly. Rational features to carry.")
+    case "crackhead":
+        print("Addiction is just the beginning of your woes.")
+
+sleep(2)
+
+race = input("Now, please select your race... are you a lizard, an elf, or a motherfucking LIVING CHANISAW?!")
+
+match race:
+    case "lizard"
+        print("Slithery, brutal, and dumb as shit, this lizard dominates nature, and certain segments of the natural world.")
+
 
 print("\n\nNow, our story begins in the Old Town of Barley Hump.")
+
---
+++ matching.py
@@ -0,0 +1,15 @@
+
+input = input("Hello Salem! What would you like to do today?\n")
+
+
+
+match input:
+    case "puke":
+        print("BRRRAAAAAAAAAHHHHHHHHPOIWERMFERJINVREAIBAENAEJKVNANCVACVWE")
+    case "drive":
+        print("No, you do not have a license.")
+    case "study":
+        print("you're probably lying but ok, go for it.")
+    case _ :
+        print("That is not one of my programmed responses.")
+
