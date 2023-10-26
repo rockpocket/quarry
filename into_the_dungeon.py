96f3c933ccafc9e46d4ba009f2aaf1d3d3912f05
rockpocket <sgtdrobnick@gmail.com>
2023-10-25 21:02:11 -0400

    oy

--- into_the_dungeon.py
+++ into_the_dungeon.py
@@ -17,7 +17,7 @@ if name == "no":
 elif (name is None):
     print("Please don't be difficult, I'm this is me working right now. Name, please")
 else:
-    print(name + ", Wonderful! What a grand name, not stupid at all.")
+    print(name + ", Wonderful! What a grand name, not stupid at all.\n")
 
 sleep(2)
 
@@ -28,11 +28,11 @@ role = role.casefold()
 #Asking the player his role
 match role:
     case "wizard":
-        print("Ah, so the spell slinging wizard, wise choice, or so, we shall see.")
+        print("Ah, so the spell slinging wizard, wise choice, or so, we shall see.\n")
     case "fighter":
-        print("Sturdy and deadly. Rational features to carry.")
+        print("Sturdy and deadly. Rational features to carry.\n")
     case "crackhead":
-        print("Addiction is just the beginning of your woes.")
+        print("Addiction is just the beginning of your woes.\n")
 
 sleep(2)
 
@@ -44,19 +44,19 @@ race = race.casefold()
 
 match race:
     case "lizard":
-        print("Slithery, brutal, and dumb as shit, this lizard dominates nature, and certain segments of the natural world.")
+        print("Slithery, brutal, and dumb as shit, this lizard dominates nature, and certain segments of the natural world.\n")
     case "elf":
-        print("Nature good. Natural world bad. Make the world better by killing racist humans. Your forest shall encroach all.")
+        print("Nature good. Natural world bad. Make the world better by killing racist humans. Your forest shall encroach all.\n")
 
 chainsaw = "chainsaw"
 
 if "chainsaw" in race:
-    print("You are chaos. You live for destruction. Viewer discretion is advised.")
+    print("You are chaos. You live for destruction. Viewer discretion is advised.\n")
 
 sleep(2)
 
 #Asking the player for their backstory
-backstory = input("And finally... what is your backstory? Are you rich, poor, or military?")
+backstory = input("And finally... what is your backstory? Are you rich, poor, or military?\n")
 
 backstory = backstory.casefold()
 
@@ -64,11 +64,11 @@ sleep(2)
 
 match backstory:
     case "rich":
-        print("Good choice. Probably the best choice, honestly. Money solves, like, every problem.")
+        print("Good choice. Probably the best choice, honestly. Money solves, like, every problem.\n")
     case "poor":
-        print("Understood. You have selected HARD MODE. Good luck.")
+        print("Understood. You have selected HARD MODE. Good luck.\n")
     case "military":
-        print("Military, of course. Who needs wealth or social status when you're capable of killing the people around you.")
+        print("Military, of course. Who needs wealth or social status when you're capable of killing the people around you.\n")
 
 sleep(2)
 
@@ -77,8 +77,18 @@ print("Now, our story begins in the Old Town of Barley Hump.\n")
 
 sleep(2)
 
-guard = input("""You are walking to the store to get some groceries, and you are approached by a guard who says\n
+match role:
+    case "wizard":
+        crime = "setting a library on fire"
+    case "fighter":
+        crime = "punching the mayor"
+    case "crackhead":
+        crime = "sucking some guys dick behind a medieval auto-parts store"
 
-this is a test of script
 
-""")
+print(f'''You are walking to the market to get some groceries, when a lone town guardsman approaches you and says
+"Hey! You! {name}! You're under arrest for {crime}, come quietly and face your judgement!\n''')
+
+guard = input("""Enter your response.
+ 
+Fight, run, talk, or yeild?\n""")
--- practice.py
+++ practice.py
@@ -1,9 +1,20 @@
-amount = 5
-iterations = 2
+#amount = 5
+#iterations = 2
 
-try:
-    average = amunt / iterations
-except ZeroDivisionError:
-    print("You can't divide by zero")
-finally:
-    print(average)
+#try:
+#    average = amunt / iterations
+#except ZeroDivisionError:
+#    print("You can't divide by zero")
+#finally:
+#    print(average)
+
+
+test = """This
+is
+printing
+a
+test"""
+
+test = str(test)
+
+print(test)
